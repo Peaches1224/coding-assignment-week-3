03ce41a56dd4eb3590896d976748972d4f28c697
Chawntel Stitts <luckypeach45@gmail.com>
2022-08-21 18:02:34 -0700

    see if I pushed

---
+++ week3assignment.js
@@ -0,0 +1,21 @@
+let ages=[];
+ages.push('3,9,23.64,2,28,93')
+
+console.log(ages[0])
+
+console.log(3-93)
+
+ ages.push[24];
+
+console.log('24')
+
+console.log(24-93)
+
+ages.forEach((ages,index)=>{
+    ages[index]= ('30.75')
+
+})
+console.log(30.75)
+
+let names=[];
+names.push('')
